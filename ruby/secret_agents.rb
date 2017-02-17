e0ebfafe88c4a907b7d72f116c3a17a59f8ed8d6
Tim Parkyn <timparkyn@gmail.com>
2017-02-17 12:11:03 -0800

    added UI, but encrypt-decrypt not working

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,19 +1,79 @@
+#encrypting process- each character moves forward one letter in alphabet"
 def encrypt (code)
-	counter = 0
-	new_code = ""
+  counter = 0
+new_code = ""
+characters = "abcdefghijklmnopqrstuvwxyz"
 
-	while counter < code.length
-		new_code = new_code + code[counter].next
-		# puts new_code
+  while counter < code.length
+  if code[counter] == "z"
+  new_code += "a"
+  else
+  new_code = new_code + code[counter].next
 
-		counter +=1
+  # new_code += characters[characters.index(code[counter]) + 1]
+  end
+  counter += 1
 
-	end
-
-	puts new_code
+  end
+   
+puts new_code
 
 end
 
-encrypt ("cyrus")
+
+#decrypting process- each character moves backwards one letter in alphabet"
+def decrypt(code)
+  counter = 0
+new_code = ""
+characters = "abcdefghijklmnopqrstuvwxyz"
+
+
+  while counter < code.length
+   
+  new_code += characters[characters.index(code[counter]) - 1] 
+  counter += 1
+
+  end
+   
+puts new_code
+
+end
+
+
+# encrypt("abc")
+# encrypt("zed")
+# decrypt("bcd")
+# decrypt("afe")
+# encrypt("swordfish")
+# decrypt("swordfish")
+
+encrypt(decrypt("swordfish"))
+
+
+
+
+
+
+#interface to ask user if they want to encrypt or decrypt
+# puts "Do you want to encrypt or decrypt?"
+  # process = gets.chomp
+# 
+# 
+  # if process == "encrypt"
+  # puts "Please enter your password."
+  # code = gets.chomp
+  # encrypt(code)
+# 
+  # elsif process == "decrypt"
+  # puts "Please enter your password."
+  # code = gets.chomp
+  # decrypt(code)
+# 
+  # else 
+  # puts "inupt unknown"
+# 
+# end
+#
+
 
 
