e6fd47d6a0bc1097e9eb57b32b9aede730db2b88
Tim Parkyn <timparkyn@gmail.com>
2017-02-17 08:43:07 -0800

    next iteration of vampire changes

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,54 +1,64 @@
-def vampire_detector (fname, age, year, bread, insurance)
+puts "How many new employees today?"
+employees = gets.chomp.to_i
+
+counter = 0
+allergies = ""
+
+while counter < employees
+
+	puts "What is your name?"
+	fname = gets.chomp
+
+	puts "How old are you?"
+	age = gets.chomp.to_i
+
+	puts "What year were you born?"
+	year = gets.chomp.to_i
+
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	bread = gets.chomp
+
+	puts "Would you like to enroll in the company’s health insurance?"
+	insurance = gets.chomp
+
+
+
+	if allergies =! "done"
+
+		puts "Do you have any suspicious allergies?"
+		allergies = gets.chomp
+
+		if allergies == "sunshine"
+			puts "Probably a vampire"
+		end
+
+		
+	end
+
+			
+
+
+
+	#vampire_detector ("juan", "20", "1984", "yes", "yes")
 
 	if age == (2017 - year) && ( bread == "yes" || insurance == "yes")
-		puts "Probably not a vampire"
+			puts "Probably not a vampire"
 
-	elsif age != (2017 - year) && ( bread != "yes" || insurance != "yes")
-		puts "Probably a vampire"
+		elsif age != (2017 - year) && ( bread != "yes" || insurance != "yes")
+			puts "Probably a vampire"
 
-	elsif age != (2017 - year) && (bread != "yes" && insurance != "yes")
-		puts "Almost certainly a vampire"
+		elsif age != (2017 - year) && (bread != "yes" && insurance != "yes")
+			puts "Almost certainly a vampire"
 
-	elsif fname == "Drake Cula" || fname == "Tu Fang"
-		puts "Definitely a vampire"
+		elsif fname == "Drake Cula" || fname == "Tu Fang"
+			puts "Definitely a vampire"
 
-	else 
-		puts "Results inconclusive"
+		else 
+			puts "Results inconclusive"
 
 	end
 
+	counter += 1
+
 end
-
-fname = ""
-age = 0
-year = 0
-bread = ""
-insurance = ""
-
-
-#puts "What is your name?"
-#fname = gets.chomp
-
-#vampire_detector
-
-#puts "How old are you?"
-#age = gets.chomp
-
-#vampire_detector
-
-#puts "What year were you born?"
-#year = gets.chomp
-
-#vampire_detector
-
-#puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-#bread = gets.chomp
-
-#vampire_detector
-
-#puts "Would you like to enroll in the company’s health insurance?"
-#insurance = gets.chomp
-
-vampire_detector ("juan", "20", "1984", "yes", "yes")
-
 
