753193e28fd829df090daf00572d141b86895dd5
Tim Parkyn <timparkyn@gmail.com>
2017-02-20 15:08:24 -0800

    added methods for the assignment

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".gsub("o", "oo")
 # => “zoom”
 
-# "enhance".<???>
+"enhance".ljust(11).rjust(14)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".last(-1)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+"z".ord
+# => 122
 
-# "How many times does the letter 'a' appear in this string?".<???>
+p "What is the significance of the number 122 in relation to the character z?"
+p "122 is is the ascii equivalent of "z"
+
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
