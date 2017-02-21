5d44bf507003a5b44eb4f1ade2c2bd64a09b52eb
Tim Parkyn <timparkyn@gmail.com>
2017-02-20 22:31:23 -0800

    added update function

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -14,10 +14,10 @@ client_info = {}
 
 
 puts "first name"
-client_info[:first_name] = gets.chomp
+client_info[:first_name] = gets.chomp.downcase
 
 puts "last name"
-client_info[:last_name] = gets.chomp
+client_info[:last_name] = gets.chomp.swapcase
 
 puts "age"
 client_info[:age] = gets.chomp.to_i
@@ -28,17 +28,25 @@ client_info[:children] = gets.chomp.to_i
 puts "likes shag?"
 client_info[:shag] = gets.chomp
 
-puts "likes MCM?"
+puts "likes mid-century modern?"
 client_info[:MCM] = gets.chomp
 
-puts "other preference"
-client_info[:other_pref] = gets.chomp
+puts "other requests?"
+client_info[:request] = gets.chomp
 
 
 
 puts client_info
 
-puts 'Do you want to make an update?'
+puts 'To make an update, enter type (enter none if done)?'
+input = gets.chomp
+
+if input != "none"
+	puts 'updated value?'
+	client_info[input] = gets.chomp
+end
+
+puts client_info
 
 
 ## build initial hash
