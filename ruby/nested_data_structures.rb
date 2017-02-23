d06490402d570b1bf6ddb5535e726e516634c064
Tim Parkyn <timparkyn@gmail.com>
2017-02-23 12:55:06 -0800

    adding additonal data structures and examples

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,3 +1,34 @@
+
+
+ride_supplies = {
+	:waterbag => 1,
+	:tubes => 2,
+	:bars => 4,
+	:tire_irons => 4,
+	:multitool=> 1,
+	:edibles => 3
+}
+
+p ride_supplies
+
+# adding to value in a hash using symbol
+ride_supplies[:tubes] += 3
+
+p ride_supplies
+
+
+
+
+bikes = ['5 spot', 'GF02', 'Synapse', 'Pubic', 'Superlight']
+
+# changing value in an array
+bikes[0] = 'five spot'
+
+p bikes[0]
+p bikes[2].upcase
+
+
+
 # campgear   
 # bigbluebag = sleeping bag, sheets, pillow, sleep pad
 # lighting = electric lantern, headlight 1, headlght 2, 4 in flashlight
@@ -7,16 +38,16 @@
 
 campgear = {
 	'bigbluebag' => [
-			'sleeping bag', 
-			'pillow', 
-			'sheets', 
-			'sleep pad'
+		'sleeping bag', 
+		'pillow', 
+		'sheets', 
+		'sleep pad'
 	],
 	'lighting' => [
 		'headlight 1', 
 		'headlight 2',
-		 '4inch flashlight',
-		 'latern' 
+		'4inch flashlight',
+		'lantern' 
 	],
 	'food and bev' => [
 		'stove',
@@ -34,28 +65,60 @@ campgear = {
 
 }
 
-
 puts campgear
 
 p ' '
 
 puts campgear['bigbluebag'][2]
 
+# replace value
 campgear['bigbluebag'][2] = 'bedsheet'
 
 p ' '
 
-puts campgear['bigbluebag'][2]
+p campgear['bigbluebag'][2]
 
+# adding a new value to array
 campgear['ondeck'].push('beverage')
 
-puts campgear
+p campgear
 
+# replacing value
 campgear['lighting'][1] = 'headlamp blue'
 
-puts campgear['lighting'][1]
+p campgear['lighting'][1]
 
-puts campgear['bigbluebag'][2].capitalize
+#adding to exiting string
+campgear['ondeck'][1] << ' gear'
+
+p campgear
+
+# capitalizing and updating an array item 
+campgear['lighting'][3].capitalize!
+
+# upcasing
+p campgear['bigbluebag'][2].upcase
+
+p campgear
+
+#checks contents to see if empty
+p campgear.empty?
+
+# changes hash into nested arrays
+a_campgear = campgear.to_a
+
+p a_campgear
+
+# printing a nested array
+p a_campgear[3]
+
+
+
+
+
+
+
+
 
 
 
