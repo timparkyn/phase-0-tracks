c0df70e95e5a746135bca882c287fa637e9e19ea
Tim Parkyn <timparkyn@gmail.com>
2017-02-25 13:08:53 -0800

    fixed string searches, added UI. hash incomplete

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,37 +13,13 @@
 # if vowel, then next_vowel
 # else next_constant
 
-# name = 'derek zoolander'
-
-# swaps first and last names
-# fake_name = name.split(' ').reverse.join(' ')
-# n = fake_name.length
-# fake_arr = fake_name.split("")
-
-# vowels = 'aeiou'
-# consts = 'bcdfghjklmnpqrstvwxyz'
-
-# until counter == fake_name.length
-# 	fake_arr[counter] ==
-
-# 	counter += 1
-# end
-
-# fake_name.map { |n| fake_name.next }
-# if 
-
-# #if letter is .include
-
-# if fake_name.include?(vowels)
 
 def name_reverse(name)
 
-	# reverses first and lase names, then becomes array
-	fake_name_arr = (name.split(' ').reverse.join(' ')).split('')
+	# downcases, reverses first and lase names, then becomes array
+	fake_name_arr = name.downcase.split(' ').reverse.join(' ').split('')
 
 	return fake_name_arr
-
-	p fake_name_arr
 
 end
 
@@ -64,9 +40,9 @@ def next_letter(letter)
 		end
 		counter += 1
 	end
-	
 
-	#searches for vowels, then replaces with next in array
+
+	#searches for consonants, then replaces with next in array
 	counter = 0
 	consts = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 
@@ -82,49 +58,80 @@ def next_letter(letter)
 		counter += 1
 	end
 
-	p new_letter
+	return new_letter
 
 end
+
+fake_name_base = {}
  
 
-# p name_reverse('derek zoolander')
+ # declaring
+name = ''
 
-name_counter = 0
-fake_name = ""
-fake_name_arr = []
+#fake_name_base = Hash.new { |name, key| flname[key] =  }
 
-name = "derek zoolander"
 
-fake_name_arr = name_reverse(name)
 
-p fake_name_arr[9]
 
-until name_counter == fake_name_arr.length
+while name != 'quit'
 
-	if fake_name_arr[name_counter] == " "
-		fake_name += " "
+	name_counter = 0
+	fake_name = ''
+	fake_name_arr = []
 
-	else
+	puts 'Create a new codename (enter quit to end)'
+	name = gets.chomp
 
-		fake_name += next_letter(fake_name_arr[name_counter])
+	if name != 'quit'
+
+		# first and last names are reversed, and split into an array of lowercase letters
+		fake_name_arr = name_reverse(name)
+
+
+		# fake_name assembly. do until the length of the array
+		until name_counter == fake_name_arr.length
+
+			# if a space is found in the array, add a space to the sting (no change)
+			if fake_name_arr[name_counter] == ' '
+				fake_name += ' '
+
+			else
+			# otherwise add next_letter to string	
+				fake_name += next_letter(fake_name_arr[name_counter])
+
+			end
+
+			name_counter += 1
+
+
+		end
+
+		#capitalize each name; split/capitalize/add back together
+		fake_name_temp = fake_name.split(' ')
+		fake_name = fake_name_temp[0].capitalize + ' ' + fake_name_temp[1].capitalize
+
+		puts fake_name
+
+		fake_name_base = {name => fake_name}
+		#fake_name_base[:flname] = name
+		#fake_name_base[:codename] =  fake_name
+
+
 
 	end
 
-	name_counter += 1
-
-
 end
 
-p fake_name.capitalize
+# fake_name_base.each do |flname, codename|
+# 	# puts flname 'is also known as' codename
+# 	# puts '{flname} is also known as #{codename}'
+# end
+
+p fake_name_base
 
 
-# p next_letter('b')
 
-# p next_letter('t')
 
-# p next_letter('z')
-
-# p next_letter('u')
 
 
 
