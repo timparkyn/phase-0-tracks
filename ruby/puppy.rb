26d992b09bcc3e2db73b2d382b854f852a690c24
Tim Parkyn <timparkyn@gmail.com>
2017-02-27 15:04:30 -0800

    added puppy characteritics

---
+++ ruby/puppy.rb
@@ -0,0 +1,24 @@
+#
+
+# species-------------
+## puppy
+
+
+# characteristics ------
+## legs: 4
+## eyes: 2
+## barks: yes
+## nose: 1
+## mood: varies
+## cuteness level: high
+
+
+
+# tricks -------
+## rollover
+## highfive
+## get ball
+## slobber
+## laydown
+## up
+## get it
