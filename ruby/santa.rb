9c81dc51f989a3430c74c924fa5c3c8b3c1b32ac
Tim Parkyn <timparkyn@gmail.com>
2017-02-28 09:35:44 -0800

    added santa stuff

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,31 +1,57 @@
-def speak
-
-	p 'Ho, ho, ho! Haaaaappy holidays!'
-
-end
-
-
-def eat_milk_and_cookies(cookie)
-
-	p 'That was a good ' + cookie + '!'
-
-end
-
-
-def initialize
-
-	p 'Initializing Santa instance...'
-
-end
+# class Santa
 
 
 
-initialize
+	def initialize(gender, ethnicity)
 
+		p 'Initializing Santa instance...'
+	
+		
+		@gender = gender
+		@ethnicity = ethnicity
+		reindeer_ranking = ['Rudolph', 'Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzer']
+		@age = 0
+
+
+	
+	end
+
+
+	def speak
+
+		p 'Ho, ho, ho! Haaaaappy holidays!'
+
+	end
+
+
+	def eat_milk_and_cookies(cookie)
+
+		puts "That was a delicious #{cookie}"
+
+	end
+
+
+# end
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+p santas.each {|x,y| puts x,  y}
+
+speak
 
 eat_milk_and_cookies('magic brownie')
 
-speak
+
+
+
+
 
 
 
