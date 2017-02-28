b4b7f59334e931085bc50b3e9e74196dfcc2307b
Tim Parkyn <timparkyn@gmail.com>
2017-02-27 21:03:54 -0800

    added R0 methods

---
+++ ruby/santa.rb
@@ -0,0 +1,34 @@
+def speak
+
+	p 'Ho, ho, ho! Haaaaappy holidays!'
+
+end
+
+
+def eat_milk_and_cookies(cookie)
+
+	p 'That was a good ' + cookie + '!'
+
+end
+
+
+def initialize
+
+	p 'Initializing Santa instance...'
+
+end
+
+
+
+initialize
+
+
+eat_milk_and_cookies('magic brownie')
+
+speak
+
+
+
+
+
+
