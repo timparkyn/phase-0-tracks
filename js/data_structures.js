f11031d74b1ed270023d752ac98ece6e3da6fd09
Tim Parkyn <timparkyn@gmail.com>
2017-03-07 11:00:38 -0800

    created horses hash

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,7 +3,7 @@
 
 var colors = ["blue", "green", "red", "purple"];
 
-var name = ["Ed", "Pokey", "Bill", "Chex Party Mix"];
+var name = ["Ed", "Pokey", "Bill", "Chex_Party_Mix"];
 
 colors.push("orange");
 
@@ -11,4 +11,12 @@ name.push("Bob");
 
 
 
+horses = {}
 
+for (var i = 0; i < name.length; i++) {
+
+	horses[name[i]] = colors[i]
+
+}
+
+console.log(horses)
